1fd20bf543de7f908ae8eaaf86792daa7bc3b267
Andrei Punko <andd3dfx@gmail.com>
2021-12-28 13:38:24 +0300

    Add demo project

---
+++ build.gradle.kts
@@ -0,0 +1,3 @@
+import by.andd3dfx.build.lib
+
+lib(version = "0.0.1")
---
+++ src/main/java/by/andd3dfx/Main.java
@@ -0,0 +1,7 @@
+package by.andd3dfx;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello, world!");
+    }
+}
